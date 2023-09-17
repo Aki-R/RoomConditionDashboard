7c2bb34e965a24ff98c326186c5e3287a3366593
Aki-R <akiakr.s@gmail.com>
2023-09-17 20:59:33 +0900

    refactoring

--- app.py
+++ app.py
@@ -1,14 +1,13 @@
 import streamlit as st
 import pandas as pd
 import pandas.tseries.offsets as offsets
-import numpy as np
 import plotly.express as px
 
 # initialize
 if "timescale" not in st.session_state:
     st.session_state.timescale = "1day"
 
-st.session_state.timescale=st.selectbox("Time Scale", ("1day", "1week"))
+st.session_state.timescale = st.selectbox("Time Scale", ("1day", "1week"))
 
 dataframe = pd.read_json("./wfirex_sensor.json")
 
@@ -18,16 +17,20 @@ datetime_latest = dataframe.tail(1)["DateTime"].values[0]
 datetime_before = datetime_latest - offsets.Hour(1)
 datetime_before_day = datetime_latest - offsets.Day(1)
 datetime_before_week = datetime_latest - offsets.Day(7)
-temperature_before = dataframe.loc[dataframe["DateTime"]>datetime_before].head(1)["Temperature"].values[0]
-humidity_before = dataframe.loc[dataframe["DateTime"]>datetime_before].head(1)["Humidity"].values[0]
+temperature_before = dataframe.loc[dataframe["DateTime"] > datetime_before].head(1)["Temperature"].values[0]
+humidity_before = dataframe.loc[dataframe["DateTime"] > datetime_before].head(1)["Humidity"].values[0]
 
 col1, col2 = st.columns(2)
 
 with col1:
-    st.metric(label="Temperature", value=f"{temperature_latest} °C", delta=f"{(temperature_latest - temperature_before):.1f} °C")
+    st.metric(label="Temperature",
+              value=f"{temperature_latest} °C",
+              delta=f"{(temperature_latest - temperature_before):.1f} °C")
 
 with col2:
-    st.metric(label="Humidity", value=f"{humidity_latest} %", delta=f"{(humidity_latest - humidity_before):.1f} %")
+    st.metric(label="Humidity",
+              value=f"{humidity_latest} %",
+              delta=f"{(humidity_latest - humidity_before):.1f} %")
 
 # Temperaturue Figure
 config = {'scrollZoom': True}
@@ -48,11 +51,13 @@ fig_temp.update_layout(dragmode='pan',
 fig_temp.update_yaxes(title="°C")
 fig_temp.update_xaxes(title=None)
 if st.session_state.timescale == "1week":
-    fig_temp.update_xaxes(range=[pd.Timestamp(datetime_before_week).to_pydatetime(), pd.Timestamp(datetime_latest).to_pydatetime()])
+    fig_temp.update_xaxes(range=[pd.Timestamp(datetime_before_week).to_pydatetime(),
+                                 pd.Timestamp(datetime_latest).to_pydatetime()])
 elif st.session_state.timescale == "1day":
-    fig_temp.update_xaxes(range=[pd.Timestamp(datetime_before_day).to_pydatetime(), pd.Timestamp(datetime_latest).to_pydatetime()])
+    fig_temp.update_xaxes(range=[pd.Timestamp(datetime_before_day).to_pydatetime(),
+                                 pd.Timestamp(datetime_latest).to_pydatetime()])
 
-st.plotly_chart(fig_temp, theme="streamlit", use_container_width=False, config=config)
+st.plotly_chart(fig_temp, theme="streamlit", use_container_width=True, config=config)
 
 # Humidity Figure
 fig_humd = px.line(
@@ -68,10 +73,10 @@ if st.session_state.timescale == "1week":
     fig_humd.update_xaxes(range=[pd.Timestamp(datetime_before_week).to_pydatetime(), pd.Timestamp(datetime_latest).to_pydatetime()])
 elif st.session_state.timescale == "1day":
     fig_humd.update_xaxes(range=[pd.Timestamp(datetime_before_day).to_pydatetime(), pd.Timestamp(datetime_latest).to_pydatetime()])
-st.plotly_chart(fig_humd, theme="streamlit", use_container_width=False, config=config)
+st.plotly_chart(fig_humd, theme="streamlit", use_container_width=True, config=config)
 
 # Illuminace
-fig_ilm= px.line(
+fig_ilm = px.line(
     dataframe,
     x="DateTime",
     y='Iluminance',
@@ -81,9 +86,11 @@ fig_ilm.update_layout(dragmode='pan')
 fig_ilm.update_yaxes(title='lx')
 fig_ilm.update_xaxes(title=None)
 if st.session_state.timescale == "1week":
-    fig_ilm.update_xaxes(range=[pd.Timestamp(datetime_before_week).to_pydatetime(), pd.Timestamp(datetime_latest).to_pydatetime()])
+    fig_ilm.update_xaxes(range=[pd.Timestamp(datetime_before_week).to_pydatetime(),
+                                pd.Timestamp(datetime_latest).to_pydatetime()])
 elif st.session_state.timescale == "1day":
-    fig_ilm.update_xaxes(range=[pd.Timestamp(datetime_before_day).to_pydatetime(), pd.Timestamp(datetime_latest).to_pydatetime()])
+    fig_ilm.update_xaxes(range=[pd.Timestamp(datetime_before_day).to_pydatetime(),
+                                pd.Timestamp(datetime_latest).to_pydatetime()])
 st.plotly_chart(fig_ilm, theme="streamlit", use_container_width=False, config=config)
 
 # pandas dataframe view
